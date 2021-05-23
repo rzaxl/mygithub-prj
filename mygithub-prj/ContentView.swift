529b2ee934543b4f9f89a388681db92843537116
moyo <moyolinux@gmail.com>
2021-05-23 23:40:55 +0100

    Initial Commit

---
+++ mygithub-prj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mygithub-prj
+//
+//  Created by moyo salami on 23/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mygithub-prj/mygithub_prjApp.swift
@@ -0,0 +1,17 @@
+//
+//  mygithub_prjApp.swift
+//  mygithub-prj
+//
+//  Created by moyo salami on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct mygithub_prjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
